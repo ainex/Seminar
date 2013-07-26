2d5e1bc4ede93f6f1a655e66b2ce63b3a211cbab
ainex <ainex.niene@gmail.com>
2013-07-26 18:32:14 +0700

    task 3 fix

--- seminar_tasks/task3/src/com/suhorukov/ulyanova/parser/TextParser.java
+++ seminar_tasks/task3/src/com/suhorukov/ulyanova/parser/TextParser.java
@@ -16,70 +16,87 @@ public class TextParser {
 
 
     public static void main (String [] args) throws IOException {
-        String inFilePath, outFilePath;
-        File inFile = new File(args[0]);
-        File outFile;
-        Reader r = new InputStreamReader(new BufferedInputStream(new FileInputStream(inFile)));
-        PrintWriter wr;
-        StringBuilder sb = new StringBuilder();
-        Map <String, Integer> wordMap = new HashMap<>();
-        List <WordCounter> wordList =new ArrayList<>();
-        WordCounter word;
-        //InputStreamReader is a bridge from byte streams to character streams
-        String str = "";
-        int i, wCounter=0;
 
-        while ((i=r.read())!=-1) {
+        if (args.length>0){
+            Reader r=null;
+            StringBuilder sb = null;
+            Map <String, Integer> wordMap = null;
+            WordCounter word;
+            int i, wCounter= 0;
+            try {
+                r = new InputStreamReader(new BufferedInputStream(new FileInputStream(new File(args[0]))));
+                sb = new StringBuilder();
+                wordMap = new HashMap<>();
+                String str = "";
+                wCounter = 0;
 
-            if (Character.isLetterOrDigit(i)) {
-                sb.append((char) Character.toLowerCase(i));
+                while ((i=r.read())!=-1) {
 
-            } else {
-                if (sb.length() != 0) {
-                    str = sb.toString();
-                    if (wordMap.containsKey(str)) {
-                        wordMap.put(str, wordMap.get(str) + 1);
+                    if (Character.isLetterOrDigit(i)) {
+                        sb.append((char) Character.toLowerCase(i));
                     } else {
-                        wordMap.put(str, 1);
+                        if (sb.length() != 0) {
+                            str = sb.toString();
+                            if (wordMap.containsKey(str)) {
+                                wordMap.put(str, wordMap.get(str) + 1);
+                            } else {
+                                wordMap.put(str, 1);
+                            }
+                            wCounter++;
+                            sb.setLength(0);
+                        }
                     }
-                    wCounter++;
-                    sb.setLength(0);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (r != null) {
+                    r.close();
                 }
             }
+
+
+            List <WordCounter> wordList =new ArrayList<>();
+            for (Map.Entry <String, Integer> entry : wordMap.entrySet()) {
+
+                wordList.add(new WordCounter (entry.getKey(), entry.getValue()));
+
+            }
+
+            Collections.sort(wordList);
+
+            File outFile=null;
+            try {
+                outFile = new File("statistics.csv");
+                System.out.println("Word analyse stat at: " + outFile.getCanonicalPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            PrintWriter wr = null;
+            try {
+                wr = new PrintWriter (new BufferedWriter (new FileWriter (outFile)));
+
+                for (Iterator it = wordList.iterator(); it.hasNext();) {
+                    word = (WordCounter) it.next();
+                    sb.setLength(0);
+                    sb.append(word.getWord()+ ";");
+                    sb.append(word.getFrequency()+";");
+                    double num =  (word.getFrequency()*100.0/wCounter);
+                    wr.write(sb.toString());
+                    wr.format("%.2f",num);
+                    wr.println();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (wr != null) {
+                    wr.close();
+                }
+            }
+
+        } else {
+            System.out.println("file path not specified");
         }
-        r.close();
-        System.out.println(wordMap);
-        System.out.println(wCounter);
-
-        for (Map.Entry <String, Integer> entry : wordMap.entrySet()) {
-
-            wordList.add(new WordCounter (entry.getKey(), entry.getValue()));
-
-        }
-
-        Collections.sort(wordList);
-
-        inFilePath = inFile.getPath();
-        outFilePath = inFilePath.replaceFirst(inFile.getName(), "statistics.csv");
-        System.out.println(outFilePath);
-        outFile = new File (outFilePath);
-        if (!outFile.createNewFile()) {
-            outFile.delete();
-            outFile.createNewFile();
-        }
-
-        wr = new PrintWriter (new BufferedWriter (new FileWriter (outFile)));
-
-        for (Iterator it = wordList.iterator(); it.hasNext();) {
-            word = (WordCounter) it.next();
-            sb.setLength(0);
-            sb.append(word.getWord()+ ";");
-            sb.append(word.getFrequency()+";");
-            double num =  (word.getFrequency()*100.0/wCounter);
-            wr.write(sb.toString());
-            wr.format("%.2f",num);
-            wr.println();
-        }
-        wr.close();
     }
 }
